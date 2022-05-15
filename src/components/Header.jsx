69924321eb45cb38f7fa6fa28b94dcb47fb1a19b
ChaMiz12 <mizcha129@gmail.com>
2022-05-15 09:27:58 +0900

    Add time

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+    setInterval(updateTime, 1000)
+    const now = new Date().toLocaleTimeString();
+    const [time, setTime] = useState(now);
+
+    function updateTime() {
+        const newTime = new Date().toLocaleTimeString()
+        setTime(newTime)
+    }
+
     return (
         <header>
-            <h1>Keeper</h1>
+            <h1>Notes Keeper {time}</h1>
         </header>
     )
 }
